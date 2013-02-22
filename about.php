e8581802c386eebda1fd4422a62393c0f1bfc928
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-02-21 20:21:30 -0800

    initialized about page

---
+++ about.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Online Book Sales</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/style.css" rel="stylesheet">
+    <style type="text/css">
+      .jumbotron {
+        margin: 80px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 100px;
+        line-height: 1;
+      }
+      .jumbotron .lead {
+        font-size: 24px;
+        line-height: 1.25;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <header>
+        <h3 class="muted">Online Book Sales</h3>
+		<div class="main-link pull-right">
+			<p>
+			<a href="login.php">login</a> / <a href="register.php">Register</a>
+			</p>
+		</div>
+        <div class="navbar navbar-inner">
+              <ul class="nav">
+                <li><a href="index.php">Home</a></li>
+                <li class="active"><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+              </ul>
+			  <div class="input-append pull-right">
+				<input class="span2" id="appendedInputButtons" type="text">
+				<button class="btn" type="button">Search</button>
+			</div>
+        </div><!-- /.navbar -->
+      </header>
+      <hr>
+
+      <!-- Example row of columns -->
+	  <div class="sub-heading">
+	  Top New Releases in Books <a href="#">(view more)</a>
+	  </div>
+	  <hr>
+		<div class="row-fluid">
+            <ul class="thumbnails">
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+			  <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+		  
+		  <hr>
+		  <div class="sub-heading">
+	  Bestselling Books <a href="#">(view more)</a>
+	  </div>
+	  <hr>
+		<div class="row-fluid">
+            <ul class="thumbnails">
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+			  <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+		  
+		  <hr>
+		  <div class="sub-heading">
+	  Special Offer <a href="#">(view more)</a>
+	  </div>
+	  <hr>
+		<div class="row-fluid">
+            <ul class="thumbnails">
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+			  <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+              <li class="span3">
+                <div class="thumbnail">
+                  <img data-src="holder.js/300x200" alt="300x200" style="width: 300px; height: 200px;" src="images/300x200.png">
+                  <div class="caption">
+                    <h3>Book Name</h3>
+                    <p>Book Description</p>
+                    <p><a href="#" class="btn">More Details</a></p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+      <hr>
+
+      <div class="footer">
+        <p>&copy; Online Book Sales 2013</p>
+      </div>
+
+    </div> <!-- /container -->
+  </body>
+</html>
