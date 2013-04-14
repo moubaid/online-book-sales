5609bcebeb91d767e045e839c3c81a4ba15ccd0f
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-04-14 10:19:30 +0530

    changed flipkart to name

--- about.php
+++ about.php
@@ -62,9 +62,9 @@
 	  <p>Hello!</p>
 <p>It's nice of you to take the time to get to know us better. Here are some things about us that we thought you might like to know.</p>
 
-<p>Flipkart went live in 2007 with the objective of making books easily available to anyone who had internet access. Today, we're present across various categories including movies, music, games, mobiles, cameras, computers, healthcare and personal products, home appliances and electronics, stationery, perfumes, toys, apparels, shoes – and still counting!</p>
+<p>Online book store went live in 2007 with the objective of making books easily available to anyone who had internet access. Today, we're present across various categories including movies, music, games, mobiles, cameras, computers, healthcare and personal products, home appliances and electronics, stationery, perfumes, toys, apparels, shoes – and still counting!</p>
 
-<p>Be it our path-breaking services like Cash on Delivery, a 30-day replacement policy, EMI options, free shipping - and of course the great prices that we offer, everything we do revolves around our obsession with providing our customers a memorable online shopping experience. Then there's our dedicated Flipkart delivery partners who work round the clock to personally make sure the packages reach on time.</p>
+<p>Be it our path-breaking services like Cash on Delivery, a 30-day replacement policy, EMI options, free shipping - and of course the great prices that we offer, everything we do revolves around our obsession with providing our customers a memorable online shopping experience. Then there's our dedicated Online book store delivery partners who work round the clock to personally make sure the packages reach on time.</p>
 
 <p>So it's no surprise that we're a favourite online shopping destination.</p>
       <hr>
